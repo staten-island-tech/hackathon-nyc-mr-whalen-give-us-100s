0b999b33b4b5d1f9d577b9db8ddb4caa2bde101f
JoannaZhang-cs <jzsunset10@gmail.com>
2025-06-13 10:17:14 -0400

    6/13

--- game.py
+++ game.py
@@ -13,12 +13,19 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Crossy Rat")
+""" new_icon = pygame.image.load("fattie.webp")
+pygame.display.set_icon(new_icon)
+ """
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Crossy Road")
 
 clock = pygame.time.Clock()
 running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 
 
@@ -65,8 +72,8 @@ class Car(pygame.sprite.Sprite):
 
 
 
-class oad(pygame.sprite.Sprite):
-    def __init__(self, y, speed):
+class Road(pygame.sprite.Sprite):
+    def __init__(self, y_position, speed):
         super().__init__()
         self.image = pygame.Surface((SCREEN_WIDTH, 100))
         self.image.fill((50, 50, 50))  # Dark gray road
@@ -109,15 +116,24 @@ player = Player()
 all_sprites.add(player)
 
 # Create car instances
-for i in range(5):
-    car = Car(y_position=i * 100 + 100, speed=CAR_SPEED)
-    all_sprites.add(car)
-    cars.add(car)
-
 for i in range(3):
     road = Road(y_position=i * 100 + 50, speed=ROAD_SPEED)
     all_sprites.add(road)
-    roads.add(road)
+
+
+class RoadIterable:
+    def __init__(self, num_roads, y_offset=50, speed=ROAD_SPEED):
+        self.num_roads = num_roads
+        self.y_offset = y_offset
+        self.speed = speed
+
+    def __iter__(self):
+        for i in range(self.num_roads):
+            yield Road(y_position=i * 100 + self.y_offset, speed=self.speed)
+
+roads = RoadIterable(3)
+for road in roads:
+    all_sprites.add(road)
 
 
 # Main game loop
@@ -132,24 +148,11 @@ while running:
 if pygame.sprite.spritecollide(player, cars, False):
         print("YOU LOST...")
         running = False
-
+        
 screen.fill(PURPLE)
 all_sprites.draw(screen)
 
 
-"""     # Handle player movement
-keys = pygame.key.get_pressed()
-if keys[pygame.K_LEFT]:
-        player.x -= player_speed
-if keys[pygame.K_RIGHT]:
-        player.x += player_speed
-if keys[pygame.K_UP]:
-        player.y -= player_speed
-if keys[pygame.K_DOWN]:
-        player.y += player_speed
-
-if player.colliderect(obstacle):
-        print("YOU LOST...") """
 
 
 pygame.display.flip()
@@ -157,7 +160,6 @@ pygame.display.flip()
 
 # Set up the game clock
 clock.tick(60)
-
 
 
 
